12f05851a55b50310db56d3f91ef7ad4263769aa
Afaq Ahmad <afaq11072001@gmail.com>
2022-01-28 16:14:46 +0500

    added express

--- node-app/app.js
+++ node-app/app.js
@@ -1,2 +1,5 @@
-var msg = "Hello, this is a simple hello world node application!";
-console.log(msg);
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get('/',(req, res) => res.send('Hello World'));
+app.listen(PORT, () => console.log(`Server listening in port ${PORT}`))
